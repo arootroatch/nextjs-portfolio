7d4be936560beb85a12d01497daa19eb7f8bf6dc
Alex Root-Roatch <arootroatch@gmail.com>
2024-01-23 18:20:48 -0600

    remove unused {data} from form action - caused a form submission error

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -34,7 +34,7 @@ export default function Contact() {
 
       <form
         action={async (formData) => {
-          const { data, error} = await sendEmail(formData);
+          const { error} = await sendEmail(formData);
           if (error) {
             toast.error(error);
             return;
