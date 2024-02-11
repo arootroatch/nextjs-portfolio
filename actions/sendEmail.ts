3d32244d6cf84d8c7d04457f0f11c3bf80299974
Alex Root-Roatch <arootroatch@gmail.com>
2024-02-10 22:28:57 -0600

    add captcha to contact form. edit sendEmail action to collect and verify captcha

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -6,12 +6,12 @@ import ContactFormEmail from "@/email/ContactFormEmail";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const getErrorMessage = (error: unknown) => {
-  let message=" ";
+  let message = " ";
   if (error instanceof Error) {
-    message=String(error.message)
+    message = String(error.message);
   } else if (error && typeof error === "object" && "message" in error) {
-    message = String(error.message)
-  } else if (typeof error === 'string'){
+    message = String(error.message);
+  } else if (typeof error === "string") {
     message = error;
   } else {
     message = "Something went wrong";
@@ -27,41 +27,64 @@ const validateString = (value: unknown, maxLength: number) => {
 };
 
 export const sendEmail = async (formData: FormData) => {
-  const message = formData.get("message");
-  const senderEmail = formData.get("senderEmail");
-  console.log(message, senderEmail);
+  const token = String(formData.get("token"));
+  console.log(token);
 
-  // simple server-side validation
-  if (!validateString(senderEmail, 500)) {
+  // const params = {"h-captcha-response": token, secret: process.env.HCAPTCHA_SECRET
+  const params = new URLSearchParams();
+  params.append('h-captcha-response',token);
+  params.append('secret', String(process.env.HCAPTCHA_SECRET));
+  
+  const response = await fetch("https://hcaptcha.com/siteverify", {
+    method: "POST",
+    body: params,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+  const json = await response.json();
+  const { success } = json;
+  if (!success) {
     return {
-      error: "Invalid sender email",
+      error: "Invalid captcha token",
     };
-  }
-  if (!validateString(message, 5000)) {
-    return {
-      error: "Invalid message",
-    };
-  }
+  } else {
+    const message = formData.get("message");
+    const senderEmail = formData.get("senderEmail");
+    console.log(message, senderEmail);
 
-  let data;
-
-  try {
-    data = await resend.emails.send({
-      from: "ContactForm <onboarding@resend.dev>",
-      to: "arootroatch@gmail.com",
-      subject: "Message from contact form",
-      reply_to: senderEmail as string,
-      react: React.createElement(ContactFormEmail, {
-        message: message as string,
-        senderEmail: senderEmail as string
-      })
-    });
-  } catch (error: unknown) {
-    return {
-      error: getErrorMessage(error),
+    // simple server-side validation
+    if (!validateString(senderEmail, 500)) {
+      return {
+        error: "Invalid sender email",
+      };
     }
-  }
-  return {
-    data,
+    if (!validateString(message, 5000)) {
+      return {
+        error: "Invalid message",
+      };
+    }
+
+    let data;
+
+    try {
+      data = await resend.emails.send({
+        from: "ContactForm <onboarding@resend.dev>",
+        to: "arootroatch@gmail.com",
+        subject: "Message from contact form",
+        reply_to: senderEmail as string,
+        react: React.createElement(ContactFormEmail, {
+          message: message as string,
+          senderEmail: senderEmail as string,
+        }),
+      });
+    } catch (error: unknown) {
+      return {
+        error: getErrorMessage(error),
+      };
+    }
+    return {
+      data,
+    };
   }
 };
--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,6 +1,7 @@
 "use client";
-import React from "react";
+import React, { useRef, useState } from "react";
 import SectionHeader from "./SectionHeader";
+import HCaptcha from "@hcaptcha/react-hcaptcha";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 import { sendEmail } from "@/actions/sendEmail";
@@ -8,6 +9,8 @@ import SubmitBtn from "./SubmitBtn";
 import toast from "react-hot-toast";
 
 export default function Contact() {
+  const [token, setToken] = useState<null | string>(null);
+  const captchaRef = useRef<HCaptcha>(null);
   const { ref } = useSectionInView("Contact");
   const { scrollY } = useScroll();
   const opacity = useTransform(scrollY, [0, 3900, 4200], [0, 0, 1]);
@@ -30,12 +33,20 @@ export default function Contact() {
 
       <form
         action={async (formData) => {
+          if (!token) {
+            toast.error(`Please complete the captcha`);
+            return;
+          }
+          formData.append("token", token);
+          
           const { error } = (await sendEmail(formData)) || {};
           if (error) {
+            captchaRef.current?.resetCaptcha();
             toast.error(error);
             return;
           }
           toast.success("Email sent successfully!");
+          captchaRef.current?.resetCaptcha();
         }}
         className="mt-10 flex flex-col dark:text-black"
       >
@@ -54,6 +65,12 @@ export default function Contact() {
           className="h-52 my-3 rounded-lg  border border-black/10 p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           placeholder="Your message"
         ></textarea>
+        <HCaptcha
+          sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY}
+          onVerify={(token) => setToken(token)}
+          // size="invisible"
+          ref={captchaRef}
+        />
         <SubmitBtn />
       </form>
     </motion.section>
