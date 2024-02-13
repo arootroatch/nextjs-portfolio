8accea073992ce27f3b80b20b2505b450229d799
Alex Root-Roatch <arootroatch@gmail.com>
2024-02-13 12:52:49 -0600

    used background image on div instead of image element

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function BackgroundImage() {
   return (
-    <div id="bg-pic" className="-z-10 fixed top-0 h-screen w-screen">
-      <Image
+    <div id="bg-pic">
+      {/* <Image
         className="relative -translate-y-6 scale-125 md:scale-125 lg:translate-y-6 lg:scale-125 xl:scale-110 2xl:scale-100 2xl:translate-y-0"
         alt="Alex Root-Roatch portrait"
         src="/lookright.jpg"
@@ -11,7 +11,7 @@ export default function BackgroundImage() {
         quality={90}
         fill={true}
         style={{objectFit:"cover"}}
-      />
+      /> */}
     </div>
   );
 }
