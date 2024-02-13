491e810c44ed4a3e6f1f10e037cff5efb747e234
Alex Root-Roatch <arootroatch@gmail.com>
2024-02-13 00:26:01 -0600

    edited styling of bacgkround image and breakpoints

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -1,17 +1,17 @@
-import Image from "next/image"
+import Image from "next/image";
 
 export default function BackgroundImage() {
   return (
     <div id="bg-pic" className="-z-10 fixed top-0 h-screen w-screen">
       <Image
+        className="relative -translate-y-6 scale-125 md:scale-110 lg:-translate-y-0 lg:scale-100"
         alt="Alex Root-Roatch portrait"
         src="/lookright.jpg"
         priority={true}
-        quality={50}
+        quality={75}
         fill={true}
         objectFit="cover"
       />
-      
     </div>
-  )
+  );
 }
