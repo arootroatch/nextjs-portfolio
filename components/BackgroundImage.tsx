498c7f2a039f24a88dbd93f4e21898d416c9f884
Alex Root-Roatch <arootroatch@gmail.com>
2024-02-13 13:00:11 -0600

    delete commeneted code from bg image

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -1,17 +1,8 @@
-import Image from "next/image";
+
 
 export default function BackgroundImage() {
   return (
     <div id="bg-pic">
-      {/* <Image
-        className="relative -translate-y-6 scale-125 md:scale-125 lg:translate-y-6 lg:scale-125 xl:scale-110 2xl:scale-100 2xl:translate-y-0"
-        alt="Alex Root-Roatch portrait"
-        src="/lookright.jpg"
-        priority={true}
-        quality={90}
-        fill={true}
-        style={{objectFit:"cover"}}
-      /> */}
     </div>
   );
 }
