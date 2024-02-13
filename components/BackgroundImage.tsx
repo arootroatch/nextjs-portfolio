138f8da24bdc19b12a71f345f946d81ee69e420f
Alex Root-Roatch <arootroatch@gmail.com>
2024-02-13 00:03:17 -0600

    put image into div instead of as the background of the div

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -1,8 +1,16 @@
-
+import Image from "next/image"
 
 export default function BackgroundImage() {
   return (
     <div id="bg-pic" className="-z-10 fixed top-0 h-screen w-screen">
+      <Image
+        alt="Alex Root-Roatch portrait"
+        src="/lookright.jpg"
+        priority={true}
+        quality={50}
+        fill={true}
+        objectFit="cover"
+      />
       
     </div>
   )
